641b0eb8e667baf8443bc524caf8063ae6d2a405
zfreedman <zfreedman92@gmail.com>
2018-06-29 18:59:59 -0700

    feat: Instantiating player and forcing continuous forward movement

---
+++ game/Assets/scripts/Game.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    // private members
+    private Player _player;
+
+	// Use this for initialization
+	private void Start () {
+        InitPlayer();
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		
+	}
+
+    // Initialize player
+    private void InitPlayer()
+    {
+        _player = GameObject.CreatePrimitive(PrimitiveType.Cube).AddComponent<Player>();
+    }
+}
---
+++ game/Assets/scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    // Private members
+    private float _wSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MovePlayer();
+	}
+
+    // Move the player
+    void MovePlayer()
+    {
+        transform.Translate(transform.forward * _wSpeed * Time.deltaTime);
+    }
+}
